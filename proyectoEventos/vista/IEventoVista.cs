097db99a47a759040aaec5d7fc6ce157644f81d8
Danirimi <danirimi222007@gmail.com>
2025-10-16 17:25:49 -0600

    vista de eventos
    vista eventos

---
+++ proyectoEventos/vista/IEventoVista.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using proyectoEventos.Modelo;
+
+namespace proyectoEventos.vista
+{
+    internal interface IEventoVista
+    {
+
+        event EventHandler AgregarEvento;
+        event EventHandler BuscarEvento;
+        event EventHandler editarEvento;
+        event EventHandler eliminarEvento;
+
+        //lectrua de datos
+        Evento AbrirEdicion();
+
+        void notify(string message);
+    }
+}
---
+++ proyectoEventos/vista/VistaEventos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoEventos.vista
+{
+    public partial class VistaEventos : Form, IEventoVista
+    {
+        //Eventos de la interfaz
+
+        public event EventHandler AgregarEvento;
+        public event EventHandler BuscarEvento;
+        public event EventHandler editarEvento;
+        public event EventHandler eliminarEvento;
+
+
+        public VistaEventos()
+        {
+            InitializeComponent();
+        }
+        public void notify(string message)
+        {
+            MessageBox.Show(message, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void VistaEventos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
